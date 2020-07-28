080e7641ba8cd1efe4169ceb053d10ed22835b99
alexey <elshin195@gmail.com>
2020-07-28 23:57:22 +0500

    first commit

---
+++ src/main/java/ru/yolshin/NewsRestApi/NewsRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.yolshin.NewsRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yolshin/NewsRestApi/NewsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yolshin.NewsRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
